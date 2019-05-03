9b5cae1b74bc4b74a0fbfc584c4c8687da49aa4f
PJC-1 <jumpzine@gmail.com>
2019-05-02 22:35:51 -0700

    cleaned up nextDay function

--- Control_Structures/dates_example/nextDay.py
+++ Control_Structures/dates_example/nextDay.py
@@ -13,15 +13,14 @@ def nextDay(year, month, day):
     Returns the year, month, day of the next day.
     Simple version: assume every month has 30 days.
     """
-    next_day = day
-    next_month = month
-    next_year = year
-    result = None
-    next_day += 1
-    if next_day > 30:
-        next_day = 1
-        next_month += 1
-    if next_month > 12:
-        next_month = 1
-        next_year += 1
-    return next_year, next_month, next_day
+    day += 1
+
+    if day > 30:
+        day = 1
+        month += 1
+
+    if month > 12:
+        month = 1
+        year += 1
+
+    return year, month, day
