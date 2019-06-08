7385e29646012593cb23b241edf0517e31e9869f
PJC-1 <jumpzine@gmail.com>
2019-06-07 23:18:24 -0700

    added sort linked list

---
+++ Data_Structures/Linked_List/sort_linked_list.py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+    def __repr__(self):
+        return str(self.value)
+
+class SortedLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, value):
+        """
+        Append a value to the Linked List in ascending sorted order
+
+        Args:
+           value(int): Value to add to Linked List
+        """
+        if self.head is None:
+            self.head = Node(value)
+            return
+
+        if value < self.head.value:
+            node = Node(value)
+            node.next = self.head
+            self.head = node
+            return
+
+        node = self.head
+        while node.next is not None and value >= node.next.value:
+            node = node.next
+
+        new_node = Node(value)
+        new_node.next = node.next
+        node.next = new_node
+
+        return None
