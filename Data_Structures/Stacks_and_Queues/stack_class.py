b7ea6267720041d1ae33b2c8b02b05f16de397e5
PJC-1 <jumpzine@gmail.com>
2019-06-11 23:05:09 -0700

    added push method to stack class

--- Data_Structures/Stacks_and_Queues/stack_class.py
+++ Data_Structures/Stacks_and_Queues/stack_class.py
@@ -4,3 +4,8 @@ class Stack:
         self.arr = [0 for _ in range(initial_size)]
         self.next_index = 0
         self.num_elements = 0
+
+    def push(self, data):
+        self.arr[self.next_index] = data
+        self.next_index += 1
+        self.num_elements += 1
