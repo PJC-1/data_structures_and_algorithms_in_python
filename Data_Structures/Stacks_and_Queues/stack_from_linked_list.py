76508d076aeff60b68485de5af0a2b6dbc16942d
PJC-1 <jumpzine@gmail.com>
2019-06-18 21:30:50 -0700

    size and is_empty methods to stack class for stack from linkedlist

--- Data_Structures/Stacks_and_Queues/stack_from_linked_list.py
+++ Data_Structures/Stacks_and_Queues/stack_from_linked_list.py
@@ -2,7 +2,7 @@ class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
-        
+
 class Stack:
 
     def __init__(self):
@@ -19,3 +19,9 @@ class Stack:
             self.head = new_node
 
         self.num_elements += 1
+
+    def size(self):
+        return self.num_elements
+
+    def is_empty(self):
+        return self.num_elements == 0
